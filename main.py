dd408328bce5ae40e6dfe3f4248ce0fea61d7802
Amit Samanta <developer.amitsamanta@gmail.com>
2021-09-08 10:44:24 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import requests
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap(app)
+
+# CREATE DATABASE
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////movies.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+# CREATE TABLE
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+    rating = db.Column(db.Float, nullable=True)
+    ranking = db.Column(db.Integer, nullable=True)
+    review = db.Column(db.String(250), nullable=True)
+    img_url = db.Column(db.String(250), nullable=True)
+
+    def __repr__(self):
+        return f'<Movie {self.title}>'
+
+
+db.create_all()
+
+
+@app.route("/")
+def home():
+    all_movies = db.session.query(Movie).all()
+    print(all_movies)
+    return render_template("index.html", movies=all_movies)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
